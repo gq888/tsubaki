8547744a0d2c7e4886e6110b8f4bc2cd31502fe6
li1005345206 <li1005345206@qq.com>
2025-10-20 11:31:40 +0800

    移除图片预加载

--- src/main.js
+++ src/main.js
@@ -2,9 +2,9 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import { preload } from "./utils/help.js";
+// import { preload } from "./utils/help.js";
 
-preload();
+// preload();
 
 const app = createApp(App);
 app.use(router);
